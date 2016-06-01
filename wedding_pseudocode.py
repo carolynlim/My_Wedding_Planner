62f3623055403fe74c573397046999f2f29460eb
Carolyn <carolynlim@carolyns-MacBook-Air.local>
2016-05-31 19:20:59 -0700

    psuedocode

---
+++ wedding_pseudocode.py
@@ -0,0 +1,18 @@
+# Introduce the user to the Application
+# Ask the user if they would like to rate a prospective guest.
+# If yes, ask the user for the prospective guest’s name and create a variable to store it.
+# If no, break.
+# Create a loop so that the questionnaire continues as long as the user says yes.
+# Create a Global Score variable so that the total score can be tallied
+# Create a function to ask a question that uses conditionals to score the user’s answers. Repeat for as many questions as desired.
+# Use local and global scope to calculate total score.
+# Use a main function to execute the program.
+# Print the guest’s name and score.
+# Store potential guest’s name and score into a dictionary.
+# Call the dictionary to return the list of potential guests and scores.
+# Separate the guests. Only the guests with a (blank) score or above should be included.
+# Parse the dictionary so that only the keys (guest’s name) appear. This is your guest list.
+
+
+# Things that I would like to add
+# Social media APIs to incorporate user’s interaction with potential guest to generate more weight to the scoring.
